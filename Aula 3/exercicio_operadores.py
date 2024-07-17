1e4011155a945672e6fe79ae6125cf7910460141
Octávio <screminoctavio@gmail.com>
2024-07-17 18:02:43 -0300

    exercicio de operadores concluído

---
+++ Aula 3/exercicio_operadores.py
@@ -0,0 +1,8 @@
+mat = float(input('Digite um valor inteiro: '))
+ing = float(input('Digite um valor inteiro: '))
+geo = float(input('Digite um valor inteiro: '))
+
+if(geo >= 7 and ing >= 7 and geo >= 7):
+    print("Parabéns, você foi aprovado!")
+else: 
+    print("Infelizmente você foi reprovado.")
